0990862d719b075868be1ec786a9f6b8259b40b4
BmDevelopers001 <bmsavaliya001@gmail.com>
2023-01-20 01:25:35 +0530

    Room Setup

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,7 +1,24 @@
 const socket = io('http://localhost:8000/');
 
-socket.on('start' , (data) => {
+// socket.on('start' , (data) => {
+//     console.log(data);
+// })
+
+const roomID = document.getElementById('roomID');
+const joinRoom = document.getElementById('joinRoom');
+joinRoom.onclick = (e) => {
+    e.preventDefault()
+
+    const RoomID = roomID.value;
+    const userID = 10
+
+    socket.emit('join-room' , {RoomID , userID} )
+
+    // console.log('joined');
+}
+
+socket.on('user-join' , (data) => {
     console.log(data);
 })
 
-socket.emit('client' , 'Hello from client')
+// socket.emit('client' , 'Hello from client')
