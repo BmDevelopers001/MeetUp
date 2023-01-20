3688d724a791c46fd772970d70b627da348ba8e3
BmDevelopers001 <bmsavaliya001@gmail.com>
2023-01-20 14:19:42 +0530

    Remote Stream Done

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const {Server} = require('socket.io');
 const http = require('http');
-const cors = require('cors')
+const cors = require('cors');
+// require('dotenv').config()
 
 const app = express()
+app.use(cors({
+    origin : '*'
+}))
 
 const httpServer =  http.createServer(app)
 
 httpServer.listen(8000 , () => {
-    console.log('Server started at 8000');
+    console.log(`Server started at 8000`);
 })
 
 const io = new Server(httpServer , {
@@ -19,11 +23,15 @@ const io = new Server(httpServer , {
 
 io.on('connection' , (socket) => {
 
-    socket.on('join-room' , (RoomID , id) => {
+    socket.on('join-room' , (RoomID , userID) => {
         // const {RoomID,userID} = data
-        console.log(RoomID , id);
+        console.log(RoomID , userID);
         socket.join(RoomID)
-        socket.to(RoomID).emit('user-join' , id)
+        socket.to(RoomID).emit('user-join' , userID)
+
+        socket.on('disconnect' , () => {
+            socket.to(RoomID).emit('user-disconnected', userID)
+        })
     })
 
 })
